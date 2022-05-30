35441e11ccb498a00d6ed611b34b96303ddab835
Rase <rasengithub@protonmail.com>
2022-05-30 16:10:40 +0300

    Renamed file

--- src/my/parse_additional_csv.rs
+++ src/my/parse_additional_csv.rs
@@ -1,16 +1,16 @@
 use std::error::Error;
+
 use csv::ReaderBuilder;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize, Eq, PartialEq)]
 struct Format {
-    database:String,
-    entries :String,
-    dumped : String,
+    database: String,
+    entries: String,
+    dumped: String,
 }
 
-
-pub fn parse_additional() -> Result<(), Box<dyn Error>> {
+pub fn parse_additional_csv() -> Result<(), Box<dyn Error>> {
     // Build the CSV reader and iterate over each record.
     let mut rdr = ReaderBuilder::new()
         .has_headers(false)
@@ -20,7 +20,7 @@ pub fn parse_additional() -> Result<(), Box<dyn Error>> {
 
     let mut additional_data: Vec<Format> = Vec::with_capacity(10000);
     for result in iter {
-        let record : Format = result?;
+        let record: Format = result?;
         additional_data.push(record);
     }
     println!("{:?}", additional_data);
