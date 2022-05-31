18113698399a4a93f9676e5dbde35f5fd76a99cf
Rase <rasengithub@protonmail.com>
2022-05-31 14:57:56 +0300

    Added test for combining data.

--- src/tests.rs
+++ src/tests.rs
@@ -2,12 +2,13 @@
 mod tests {
     // use crate::my::parse_additional_csv::Format;
 
+    use crate::my::combine::combine;
     use crate::my::parse_additional_csv::parse_additional_csv;
     use crate::my::parse_existing_json::parse_existing_json;
 
     #[test]
     fn additional_csv_parsing() {
-        let expected_data_length: u16 = 3393;
+        let expected_data_length: u16 = 3394;
 
         // Vec<Format>
         let resulting_data = parse_additional_csv().expect("Error parsing csv");
@@ -26,5 +27,12 @@ mod tests {
         assert_eq!(expected_data_length, resulting_data.len() as u16);
 
         assert!(resulting_data[0]["email"].as_str().expect("Error turning value into string").len() > 0);
+    }
+
+    #[test]
+    pub fn combine_data() {
+        let combined = combine("./data/tests/example_existing.txt");
+
+        assert!(combined[0].breach_date.len() > 0);
     }
 }
