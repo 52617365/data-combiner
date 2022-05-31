0a6e14fc19cea3adccd85ae286378190379e5227
Rase <rasengithub@protonmail.com>
2022-05-31 11:06:55 +0300

    Turned json string into object so it can be better modified

--- src/my/parse_existing_json.rs
+++ src/my/parse_existing_json.rs
@@ -1,15 +1,15 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
-
 use serde_json::Value;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-pub fn parse_existing_json(filename: &str) -> Vec<Value> {
+pub fn parse_existing_json(filename: &str) -> Vec<serde_json::Map<String, Value>> {
     let lines = read_file(filename);
-    let mut json_lines: Vec<Value> = Vec::with_capacity(lines.len());
+    let mut json_lines: Vec<serde_json::Map<String, Value>> = Vec::with_capacity(lines.len());
 
     for line in lines.iter() {
-        let parsed: Value = serde_json::from_str(line).expect("Error turning line into json, check your format.");
-        json_lines.push(parsed);
+        let parsed: Value =
+            serde_json::from_str(line).expect("Error turning line into json, check your format.");
+        json_lines.push(parsed.as_object().expect("Error turning json string into object.").clone());
     }
     json_lines
 }
