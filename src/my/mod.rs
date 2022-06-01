f4f74fc17c331df679e2793fe259e6699c27ca7f
Rase <rasengithub@protonmail.com>
2022-06-01 09:03:16 +0300

    Started making writing file that writes contents in csv.

--- src/my/mod.rs
+++ src/my/mod.rs
@@ -2,3 +2,4 @@
 pub mod parse_additional_csv;
 pub mod parse_existing_json;
 pub mod combine;
+pub mod write_as_csv;
---
+++ src/my/write_as_csv.rs
@@ -0,0 +1,6 @@
+use std::io::Result;
+
+pub fn write_as_csv() -> Result<()> {
+
+    Ok(())
+}
